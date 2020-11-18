0742fb83dd3171d010c835f833072caa1559835d
cmarsa <marcodelossantos@protonmail.com>
2020-11-18 16:35:04 -0600

    First two programs: hwlloworld and prompting input.

---
+++ src/elements/howareyou/HowAreYou.java
@@ -0,0 +1,9 @@
+package elements.howareyou;
+
+public class HowAreYou {
+    public static void main(String[] args) {
+        System.out.println("Hi, ");
+        System.out.println(args[0]);
+        System.out.println(". How are you?");
+    }
+}
