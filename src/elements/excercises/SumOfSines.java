4542ba9bfbae1f1826d4b1581a8725eac4ebda2e
cmarsa <marcodelossantos@protonmail.com>
2020-11-18 17:58:34 -0600

    Sum of sines.

--- src/elements/excercises/SumOfSines.java
+++ src/elements/excercises/SumOfSines.java
@@ -1,2 +1,9 @@
-package elements.excercises;public class SumOfSines {
+package elements.excercises;
+
+public class SumOfSines {
+    public static void main(String[] args) {
+        double t = Double.parseDouble(args[0]);
+        double result = Math.sin(2*t) + Math.sin(3*t);
+        System.out.println("sin(2*" + t + ") + sin(3*" + t + ") = " + result);
+    }
 }
