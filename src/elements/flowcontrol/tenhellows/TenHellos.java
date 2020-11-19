cb73530c7097398d104c8e30cd92100a7859cd88
cmarsa <marcodelossantos@protonmail.com>
2020-11-19 11:51:48 -0600

    using while in flow control.

---
+++ src/elements/flowcontrol/tenhellows/TenHellos.java
@@ -0,0 +1,16 @@
+package elements.flowcontrol.tenhellows;
+
+public class TenHellos {
+    public static void main(String[] args) {
+        // print ten hellos
+        System.out.println("1st Hello");
+        System.out.println("2st Hello");
+        System.out.println("3st Hello");
+        int i = 4;
+        while (i <= 10) {
+            System.out.println(i + "th Hello");
+            i += 1;
+        }
+        return;
+    }
+}
