041a2ca42c0eba17377360c75b7466817512a88d
cmarsa <marcodelossantos@protonmail.com>
2020-11-18 18:50:46 -0600

    random number generation from gaussian dist N(0, 1).

---
+++ src/elements/excercises/RandomGaussian.java
@@ -0,0 +1,12 @@
+package elements.excercises;
+
+public class RandomGaussian {
+    public static void main(String[] args) {
+        // get pseudo random numbers from Math API
+        double u = Math.random();
+        double v = Math.random();
+        // compute a random number from the gaussian dist
+        double r = Math.sin(2*Math.PI*v) * Math.pow(-2*Math.log(u), 0.5);
+        System.out.println(r);
+    }
+}
