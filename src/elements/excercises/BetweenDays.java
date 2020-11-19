63279bad71b638452b697ea2b58f91163de079dd
cmarsa <marcodelossantos@protonmail.com>
2020-11-18 18:21:10 -0600

    Is between days problem.

--- src/elements/excercises/BetweenDays.java
+++ src/elements/excercises/BetweenDays.java
@@ -1,2 +1,34 @@
-package elements.excercises;public class BetweenDays {
+package elements.excercises;
+
+public class BetweenDays {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Please provide params\nd[day], m[month].");
+            return;
+        }
+        int d = Integer.parseInt(args[0]);
+        int m = Integer.parseInt(args[1]);
+        boolean isBetween = false;
+        if (m >= 3 && m <= 6) {
+            if (m == 3) {
+                if (d < 20)
+                    System.out.println("false");
+                else
+                    if (d > 0)
+                        System.out.println("true");
+            }
+            else if (m == 6) {
+                if (d > 20)
+                    System.out.println("false");
+                else
+                    System.out.println("true");
+            }
+            else {
+                System.out.println("true");
+            }
+        }
+        else {
+            System.out.println("false");
+        }
+    }
 }
