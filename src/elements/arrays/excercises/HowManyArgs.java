2c1e9ccf7abc3f653f29cc2df4ee22c2bc946958
mar <mar.dls@tutamail.com>
2021-02-13 18:28:46 -0600

    Count input arguments to program.

---
+++ src/elements/arrays/excercises/HowManyArgs.java
@@ -0,0 +1,8 @@
+package elements.arrays.excercises;
+
+public class HowManyArgs {
+    public static void main(String[] args) {
+        int n = args.length;
+        System.out.println("number of args: " + n);
+    }
+}
